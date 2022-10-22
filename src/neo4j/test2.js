750de4a82e873c4b26ac51c26b521707fa44d231
sharzy720 <jstrong1@catamount.wcu.edu>
2022-10-21 20:42:01 -0400

    Draft of creating json files based on data received from the database

--- src/neo4j/test2.js
+++ src/neo4j/test2.js
@@ -13,7 +13,7 @@
     const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 
     try {
-        idFile = fs.openSync('../JSONFiles/ids_test.json', 'w');
+        idFile = fs.openSync('test_ids.json', 'w');
         transactionFile = fs.openSync('test.json', 'w');
         await getUserIds(driver);
         // await getSourceIds(driver);
@@ -36,6 +36,9 @@
 
         const session = driver.session({ database: 'neo4j' });
 
+        // TODO create array of all user ids including duplicate ids
+        let userIDS = []
+
         try {
             const readQuery = 'MATCH p = (S:source)-[* {\`time step\`:' + timeStep + '}]-(T:target) RETURN S.name, T.name';
 
@@ -45,30 +48,42 @@
 
             // console.log(readResult.records.toLocaleString())
 
-            // TODO create array of all user ids including duplicate ids
-            let userIDS = []
 
+            // TODO create json of transactions (source, target)
             writeToFile(transactionFile, "[\n")
 
             console.log("length of result" + readResult.records.length)
 
-            readResult.records.forEach(record => {
+            for await (const record of readResult.records) {
                 // const test = record.get('n')
                 console.log(`Source ID: ${record.get('S.name')} ----> Target ID: ${record.get('T.name')}`)
 
                 let sourceID = record.get('S.name');
-                let targetID = record.get('T.name');
+                userIDS.push(sourceID.toString());
 
-                // TODO create json of transactions (source, target)
+                let targetID = record.get('T.name');
+                userIDS.push(targetID.toString());
+
                 appendToFile(transactionFile, "{ \"source\": " + sourceID + ", \"target\": " + targetID + " },\n")
 
-            });
+            }
 
+            // readResult.records.forEach(record => {
+            //     // const test = record.get('n')
+            //     console.log(`Source ID: ${record.get('S.name')} ----> Target ID: ${record.get('T.name')}`)
+            //
+            //     let sourceID = record.get('S.name');
+            //     userIDS.push(sourceID);
+            //
+            //     let targetID = record.get('T.name');
+            //     userIDS.push(targetID);
+            //
+            //     appendToFile(transactionFile, "{ \"source\": " + sourceID + ", \"target\": " + targetID + " },\n")
+            //
+            // });
             appendToFile(transactionFile, "]")
-            // TODO remove duplicate user ids
 
 
-            // TODO create json of user ids (name)
 
 
         } catch (error) {
@@ -76,6 +91,62 @@
         } finally {
             await session.close();
         }
+
+        createIdJson(userIDS)
+
+        // // TODO remove duplicate user ids
+        // // userIDS = [...new Set(userIDS)];
+        // userIDS.reduce((p, c) => p.set(c.a, c), new Map()).values()
+        //
+        // console.log(userIDS.toString())
+        //
+        // // // TODO create json of user ids (name)
+        // writeToFile(idFile, "[\n")
+        //
+        // for await (const id of userIDS) {
+        //     appendToFile(idFile, "{ \"name\": " + id + " },\n");
+        // }
+        //
+        // // userIDS.forEach(id => {
+        // //     appendToFile(idFile, "{ \"name\": " + id + " },\n");
+        // // });
+        //
+        // appendToFile(idFile, "]");
+    }
+
+    function createIdJson(ids) {
+        // TODO remove duplicate user ids
+        // userIDS = [...new Set(userIDS)];
+        // let userIds = ids.reduce((p, c) => p.set(c.a, c), new Map()).values()
+        // let userIds = [...new Set(ids)]
+        // let userIds = ids.filter((element, index) => {
+        //     return ids.indexOf(element) === index;
+        // });
+        console.log("length of ids: " + ids.length)
+        let userIds = []
+        ids.forEach((c) => {
+            if (!userIds.includes(c)) {
+                userIds.push(c);
+            }
+        });
+        console.log("length of ids after removing duplicates: " + userIds.length)
+
+
+
+        // console.log(userIds.toString())
+
+        // // TODO create json of user ids (name)
+        writeToFile(idFile, "[\n")
+
+        for (const id of userIds) {
+            appendToFile(idFile, "{ \"name\": " + id + " },\n");
+        }
+
+        // userIDS.forEach(id => {
+        //     appendToFile(idFile, "{ \"name\": " + id + " },\n");
+        // });
+
+        appendToFile(idFile, "]");
     }
 
     /**
@@ -86,7 +157,7 @@
         fs.writeFile(file, message, (err) => {
             if (err) throw err;
             else{
-                console.log("The file is updated with the given data")
+                // console.log(file + ' has been reset with ' + message)
             }
         })
     }
@@ -99,7 +170,7 @@
     function appendToFile(file, message) {
         fs.appendFile(file, message, function (err) {
             if (err) throw err;
-            console.log('Saved!');
+            // console.log('Appended ' + message + ' to ' + file);
         });
     }
 
