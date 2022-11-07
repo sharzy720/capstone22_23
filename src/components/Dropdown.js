69b03247eb4d1669d3bf8ef813a5c98ed3d1d557
sharzy720 <jstrong1@catamount.wcu.edu>
2022-11-06 19:14:10 -0500

    Added a 4th menu option for selecting timesteps from the dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -26,6 +26,7 @@ function Dropdown (props) {
                     <MenuItem value={1}>1</MenuItem>
                     <MenuItem value={2}>2</MenuItem>
                     <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
                 </Select>
             </FormControl>
         </Box>
