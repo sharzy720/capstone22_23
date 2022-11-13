0319259bf2f71a7d021a487f273ab7a78ddc9a66
sharzy720 <jstrong1@catamount.wcu.edu>
2022-11-13 12:17:16 -0500

    Made it so this panel only display the visualization and no text. Also, removed unneeded commented out code

--- src/components/VisualizationPanel.js
+++ src/components/VisualizationPanel.js
@@ -1,38 +1,29 @@
 
 import React from 'react'
-import {Paper, Container} from "@mui/material";
-import D3Visualization from "./D3Visualization";
-//import ForceGraphVis from "./ForceGraphVis";
-import Vis1 from './Vis1.js';
+import {Paper} from "@mui/material";
 import JSONForceGraph from "./JSONForceGraph";
 
-//TODO fill with all things visualization
-//TODO on larger screens the visualization container does not take up the full screen
 
 function VisualizationPanel(props) {
     return (
         <Paper style={{backgroundColor: "lightblue",
-                       height: "98vh",
+                       minHeight: "98.5vh",
                        width: "100%"}}>
-            <div style={{padding: "20px"}}>
-                Visualization
-                <br/>
-                <p>
-                    The visualization of our dataset will be placed here
-                </p>
-            </div>
+            {/*<div style={{padding: "20px"}}>*/}
+            {/*    Visualization*/}
+            {/*    <br/>*/}
+            {/*    <p>*/}
+            {/*        The visualization of our dataset will be placed here*/}
+            {/*    </p>*/}
+            {/*</div>*/}
 
-            {/*Container for the graph visualization*/}
+            {/*/!*Container for the graph visualization*!/*/}
             <div
                 id={'visContainer'}
                 style={{ height: "85%",
                         width: "100%",
                         padding: "0px"}}>
                 <JSONForceGraph timestep={props.timestep} />
-                {/*<Vis1/>*/}
-                {/*<ForceGraphVis/>*/}
-                {/*<JSONForceGraph width={1150} height={1150}/>*/}
-                {/*<JSONForceGraph/>*/}
             </div>
         </Paper>
     );
