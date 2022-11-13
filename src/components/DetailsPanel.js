e0949714a36b38d5dea4ea9515c84bd9aacd0523
sharzy720 <jstrong1@catamount.wcu.edu>
2022-11-13 12:19:33 -0500

    Redesigned the details panel. Changed the background color to match the visualization panel, updated all the text sections with real details instead of test text, added a second drop down for the selecting of how many transactions to display, and added a footer with the creators names

--- src/components/DetailsPanel.js
+++ src/components/DetailsPanel.js
@@ -1,37 +1,69 @@
-
 import React from 'react'
 import {Paper} from "@mui/material";
-import Button from "./Button";
-import Dropdown from "./Dropdown";
+import DisplayButton from "./DisplayButton";
+import TimestepDropdown from "./TimestepDropdown";
+import LimitDropdown from "./LimitDropdown";
 
-//TODO fill with all information about the dataset
-
-var numNodes = 200000; //Var so you can change it later
+const numNodes = 4000; // Temp data value for amount of nodes in the graph
 
 function DetailsPanel(props) {
-    {/*const [counter, setCounter] = React.useState(10);*/}
-    {/*const incrementCounter = () => setCounter(counter+1);*/}
     const [timeStep, setTimeStep] = React.useState('1');
+    const [limit, setLimit] = React.useState('500')
 
     const updateValue = () => props.setGraph(timeStep);
     return (
-        <Paper style={{backgroundColor: "lightcoral", height: "98vh"}}>
-            <div style={{padding: "20px"}}>
-                Elliptic dataset
-                <br/>
-                <div>Number of nodes - {numNodes}</div>
+        // old background color 'lightcoral
+        <Paper style={{
+            backgroundColor: 'lightblue',
+            minHeight: '98.5vh'
+        }}>
+            <div style={{padding: '20px'}}>
+
+                {/* Title */}
+                <h1 style={{ margin: '0'}}>
+                    Elliptic dataset
+                </h1>
 
                 <br/>
-                <p>
-                    All information pertaining to the elliptic dataset will be
-                    put here
+
+                {/* sub information */}
+                <h4>Number of transactions in database: {numNodes}</h4>
+
+                {/*<br/>*/}
+
+                {/* Details about the dataset */}
+                <p style={{ fontSize: '16px' }}>
+                    The Elliptic dataset is a collection of transactions made using bitcoin taken over the course of 50 time steps.
+                    Each time step has around 7,000 unique transactions. However, our subset of this dataset only has 1,000 unique transactions per time step.
                 </p>
 
-                <Dropdown setTime_step={setTimeStep} timeStep={timeStep} />
-                <Button onClickFunction={updateValue}  label={props.graph} />
+                {/* Selecting the time step to display */}
+                <TimestepDropdown setTime_step={setTimeStep} timeStep={timeStep} />
+                {/*<DisplayButton onClickFunction={updateValue} label={props.graph} />*/}
 
-                {/*<Button onClickFunction={incrementCounter} label={counter} />*/}
-                {/*<Button onClickFunction={incrementCounter} label={"Pineapple"} />*/}
+                <br/>
+                {/*<br/>*/}
+
+                {/* Selecting the number of transactions to display */}
+                <LimitDropdown setLimit_step={setLimit} limit={limit} />
+
+                <br/>
+
+                {/* Button to display a graph with the users selected parameters */}
+                <DisplayButton onClickFunction={updateValue} label={props.graph} />
+
+                {/* Footer */}
+                {/*<div style={{*/}
+                {/*    display: 'flex',*/}
+                {/*    justifyContent: 'flex-end',*/}
+                {/*    alignItems: 'flex-end'*/}
+                {/*}}>*/}
+                    <h4 style={{
+                        position: 'absolute',
+                        bottom: '0'
+                    }}>Developed by Johnathyn Strong and Nick Wofford</h4>
+                {/*</div>*/}
+
             </div>
         </Paper>
     );
