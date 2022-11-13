bcf6e9da57b500d1905e2943b658dc98ef967678
sharzy720 <jstrong1@catamount.wcu.edu>
2022-11-13 12:11:47 -0500

    Set the minimum width and height to 1280X720 so lower resolution screens won't break the layout. Also, shrunk the details panel by 1 and increased the visualization panel by 1

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import Grid from '@mui/material/Grid'
 import DetailsPanel from "./components/DetailsPanel";
 import VisualizationPanel from "./components/VisualizationPanel";
 
-//TODO get container to have no extra spacing around it
 
 const App = () => {
     const [graph, setGraph] = React.useState('1');
 
     return (
-        <div style={{backgroundColor: "black"}}>
+        <div style={{
+            backgroundColor: "black",
+            minWidth: '1280px',
+            minHeight: '720px'
+        }}>
             <Grid
                 container
                 direction={"row"}
                 spacing={{ md: 1, xl: 2}}
                 style={{padding: "10px"}}>
 
-                <Grid item md={8}>
+                <Grid item md={9}>
                     <VisualizationPanel timestep={graph}/>
 
                 </Grid>
-                <Grid item md={4}>
+                <Grid item md={3}>
                     <DetailsPanel graph={graph} setGraph={setGraph} />
 
                 </Grid>
