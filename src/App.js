b646f8453af32711075888b4b1f8fb6aeb8c260c
sharzy720 <jstrong1@catamount.wcu.edu>
2022-12-08 16:48:38 -0500

    Added detailed comments throughout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+/**
+ * @file The root element of the app
+ * @author Johnathyn Strong and Nickolas Wofford
+ */
 
 import React from 'react'
 import Grid from '@mui/material/Grid'
 import DetailsPanel from "./components/DetailsPanel";
 import VisualizationPanel from "./components/VisualizationPanel";
 
-
+/**
+ * creates the root element of the app
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const App = () => {
-    const [graph, setGraph] = React.useState('51');
+    /**
+     * User selected timestep of transactions to query
+     * @type {Number}
+     */
+    const [timestep, setTimestep] = React.useState('51');
+
+    /**
+     * User selected number of transactions to return from the database
+     * @type {Number}
+     */
     const [limit, setLimit] = React.useState('125');
 
     return (
@@ -22,11 +40,13 @@ const App = () => {
                 style={{padding: "10px"}}>
 
                 <Grid item md={9}>
-                    <VisualizationPanel timestep={graph} limit={limit}/>
+
+                    <VisualizationPanel timestep={timestep} limit={limit}/>
 
                 </Grid>
                 <Grid item md={3}>
-                    <DetailsPanel graph={graph} setGraph={setGraph} limit={limit} setLimit={setLimit} />
+
+                    <DetailsPanel timestep={timestep} setTimestep={setTimestep} limit={limit} setLimit={setLimit} />
 
                 </Grid>
             </Grid>
