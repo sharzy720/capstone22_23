86e8a72b91e72a82cf6b599a0b6bfdb4543f91d2
sharzy720 <jstrong1@catamount.wcu.edu>
2023-02-02 12:30:52 -0500

    Changed useStates from single values to objects that hold the timestep and limit of each possible displayed graph. Also, moved everything contained within the DetailsPanel into the new DetailsDrawer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import React from 'react'
 import Grid from '@mui/material/Grid'
 import DetailsPanel from "./components/DetailsPanel";
 import VisualizationPanel from "./components/VisualizationPanel";
+import ButtonAppBar from './components/AppBar';
 
 /**
  * creates the root element of the app
@@ -17,38 +18,55 @@ import VisualizationPanel from "./components/VisualizationPanel";
 const App = () => {
     /**
      * User selected timestep of transactions to query
-     * @type {String, Function}
+     * @type {Object, Function}
      */
-    const [timestep, setTimestep] = React.useState('51');
+    const [timestep, setTimestep] = React.useState({
+        graph1: '51',
+        graph2: '51',
+        graph3: '51',
+        graph4: '51'
+    });
 
     /**
      * User selected number of transactions to return from the database
-     * @type {String, Function}
+     * @type {Object, Function}
      */
-    const [limit, setLimit] = React.useState('125');
+    const [limit, setLimit] = React.useState({
+        graph1: '125',
+        graph2: '125',
+        graph3: '125',
+        graph4: '125'
+    });
+
+    console.log("====INTIAL VALUES OF LIMIT AND TIMESTEP====")
+    console.log("TIMESTEP == " + timestep.graph1)
+    console.log("LIMIT == " + limit.graph1)
 
     return (
         <div style={{
-            backgroundColor: "black",
-            minWidth: '1280px',
-            minHeight: '720px'
-        }}>
+            // backgroundColor: "black",
+            // minWidth: '1280px',
+            // minHeight: '720px',
+            // width: '50vw'
+            }}>
+            <ButtonAppBar timestep={timestep} setTimestep={setTimestep} limit={limit} setLimit={setLimit}/>
             <Grid
-                container
-                direction={"row"}
-                spacing={{ md: 1, xl: 2}}
-                style={{padding: "10px"}}>
+                // container
+                // direction={"row"}
+                // spacing={{ md: 1, xl: 2}}
+                // style={{padding: "10px"}}
+            >
 
                 <Grid item md={9}>
 
-                    <VisualizationPanel timestep={timestep} limit={limit}/>
+                    <VisualizationPanel timestep={timestep.graph1} limit={limit.graph1}/>
 
                 </Grid>
-                <Grid item md={3}>
+                {/*<Grid item md={3}>*/}
 
-                    <DetailsPanel timestep={timestep} setTimestep={setTimestep} limit={limit} setLimit={setLimit} />
+                {/*    <DetailsPanel timestep={timestep} setTimestep={setTimestep} limit={limit} setLimit={setLimit} />*/}
 
-                </Grid>
+                {/*</Grid>*/}
             </Grid>
       </div>
     );
