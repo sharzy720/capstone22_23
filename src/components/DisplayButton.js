f8981df4468f094d9d5e23431e45fea308a344dc
sharzy720 <jstrong1@catamount.wcu.edu>
2023-02-02 12:43:32 -0500

    Added extra margin to the bottom of the button as well as making the height the same as the dropdown menus

--- src/components/DisplayButton.js
+++ src/components/DisplayButton.js
@@ -15,8 +15,9 @@ function DisplayButton(props) {
     
     return (
         <Button variant="contained" onClick={props.onClickFunction} style={{
-            height: '100%', width: '100%',
+            height: '50px', width: '100%',
             marginTop: '4px',
+            marginBottom: '4px',
             backgroundColor: 'white', color: 'black'
         }}>
             Display
