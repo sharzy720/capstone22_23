4e21f28d5f4f2496b5bd6aeec2f4593863704588
sharzy720 <jstrong1@catamount.wcu.edu>
2023-02-06 15:25:38 -0500

    Updated onClick function to allow for a value to be passed to it

--- src/components/RemoveGraphButton.js
+++ src/components/RemoveGraphButton.js
@@ -8,13 +8,14 @@ import {Button} from "@mui/material";
 /**
  * Creates a button to update the visualization using the user selected values
  * @param {Function} props.onClickFunction
+ * @param {Number} props.graphNum
  * @returns {JSX.Element}
  * @constructor
  */
 function RemoveGraphButton(props) {
-
+//props.onClickFunction(props.graphNum)
     return (
-        <Button variant="contained" onClick={props.onClickFunction} style={{
+        <Button variant="contained" onClick={() => props.onClickFunction(props.graphNum)} style={{
             height: '50px', width: '100%',
             marginTop: '4px',
             backgroundColor: 'IndianRed', color: 'black'
