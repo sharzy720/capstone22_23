986973ec1f23c39b4f63e3894e08fe3da99352d2
sharzy720 <jstrong1@catamount.wcu.edu>
2023-02-11 16:25:50 -0500

    General cleaning and commenting of the code as well as making the background of each option be the color described by the option. Replaced the white background behind the color options with a gradient background that goes from the lightest color to the darkest color eliminating the white boxes at the top and bottom of the choices.

--- src/components/ColorDropdown.js
+++ src/components/ColorDropdown.js
@@ -1,5 +1,5 @@
 /**
- * @file Dropdown menu for Limits
+ * @file Dropdown menu for visualization background color
  * @author Johnathyn Strong and Nickolas Wofford
  */
 import * as React from 'react';
@@ -12,15 +12,12 @@ import {useEffect} from "react";
 
 /**
  * Creates a drop-down menu for selecting a timestep to visualize
- * @param {Function} props.setLimit
- * @param {String} props.limit
  * @param {String} props.graph
+ * @param {String} props.vizColor
+ * @param {Function} props.setVizColor
  * @returns {JSX.Element}
  */
 function ColorDropdown (props) {
-
-    // console.log("====TIMESTEP DROPDOWN PROP VALUES====")
-    // console.log("props.limit.graph1 == " + JSON.stringify(props.limit))
 
     /**
      * User selected number of transactions
@@ -28,17 +25,24 @@ function ColorDropdown (props) {
      */
     const [color, setColor] = React.useState("")
 
+    /**
+     * Background color options for the visualizations, colors should go from light to dark
+     * @type {*[String]}
+     */
+    const vizBackground = ['Snow', 'LightSalmon', 'DarkSeaGreen', 'SteelBlue', 'SlateGrey', 'Black']
 
+
+
+    /**
+     * If the user has already selected a background color then that color will be reselected
+     */
     useEffect(() => {
         console.log("testing received limit value")
-        if (parseInt(props.color) < 9999) {
-            console.log("value acceptable")
-            setColor(props.color);
-        } else {
-            console.log("value is too small")
-            setColor("");
+        if (props.vizColor !== '9999') {
+            console.log("Color acceptable")
+            setColor(props.vizColor);
         }
-    }, [props.color]);
+    }, [props.vizColor]);
 
     /**
      * Event handle for user interacting with the drop-down.
@@ -47,22 +51,22 @@ function ColorDropdown (props) {
     const handleChange = (event) => {
         switch (props.graph) {
             case "1":
-                props.setColor(previousState => {
+                props.setVizColor(previousState => {
                     return { ...previousState, graph1: event.target.value }
                 });
                 break;
             case "2":
-                props.setColor(previousState => {
+                props.setVizColor(previousState => {
                     return { ...previousState, graph2: event.target.value }
                 });
                 break;
             case "3":
-                props.setColor(previousState => {
+                props.setVizColor(previousState => {
                     return { ...previousState, graph3: event.target.value }
                 });
                 break;
             case "4":
-                props.setColor(previousState => {
+                props.setVizColor(previousState => {
                     return { ...previousState, graph4: event.target.value }
                 });
                 break;
@@ -71,7 +75,7 @@ function ColorDropdown (props) {
     };
 
     return (
-        <Box sx={{ minWidth: 120, height: '50px' }}>
+        <Box sx={{ height: '50px' }}>
             <FormControl fullWidth variant={"filled"}>
                 <InputLabel id="color-select-label">Background Color</InputLabel>
                 <Select
@@ -80,13 +84,42 @@ function ColorDropdown (props) {
                     value={color}
                     label="color"
                     onChange={handleChange}
+                    inputProps={{
+                        MenuProps: {
+                            MenuListProps: {
+                                sx: {
+                                    // Color of the background behind the menu options
+                                    background: "linear-gradient(" + vizBackground[0] + ", " + vizBackground[5] + ")"
+                                }
+                            }
+                        }
+                    }}
                 >
-                    <MenuItem value={'PapayaWhip'}>PapayaWhip</MenuItem>
-                    <MenuItem value={'Snow'}>Snow</MenuItem>
-                    <MenuItem value={'SteelBlue'}>SteelBlue</MenuItem>
-                    <MenuItem value={'SlateGrey'}>SlateGrey</MenuItem>
-                    <MenuItem value={'Black'}>Black</MenuItem>
-                    <MenuItem value={'DarkSeaGreen'}>DarkSeaGreen</MenuItem>
+                    <MenuItem value={vizBackground[0]} style={{
+                        backgroundColor: vizBackground[0]
+                    }}>{vizBackground[0]}</MenuItem>
+
+                    <MenuItem value={vizBackground[1]} style={{
+                        backgroundColor: vizBackground[1]
+                    }}>{vizBackground[1]}</MenuItem>
+
+                    <MenuItem value={vizBackground[2]} style={{
+                        backgroundColor: vizBackground[2]
+                    }}>{vizBackground[2]}</MenuItem>
+
+                    <MenuItem value={vizBackground[3]} style={{
+                        backgroundColor: vizBackground[3]
+                    }}>{vizBackground[3]}</MenuItem>
+
+                    <MenuItem value={vizBackground[4]} style={{
+                        backgroundColor: vizBackground[4]
+                    }}>{vizBackground[4]}</MenuItem>
+
+                    <MenuItem value={vizBackground[5]} style={{
+                        backgroundColor: vizBackground[5],
+                        color: 'white'
+                    }}>{vizBackground[5]}</MenuItem>
+
                 </Select>
             </FormControl>
         </Box>
