441dc2bd7ceac67b20df7cf451d25e2ab647d9b6
Johnathyn <johnathynstrong@gmail.com>
2023-02-16 11:16:49 -0500

    Added pop out div that displays the id of the currently hovered over node

--- src/components/JSONForceGraph.js
+++ src/components/JSONForceGraph.js
@@ -81,6 +81,13 @@ function JSONForceGraph(props) {
                 });
 
             /**
+             * Pop out div for displaying a nodes id
+             */
+            var div = d3.select("body").append("div")
+                .attr("class", "node-details")
+                .style("opacity", 0);
+
+            /**
              * Node data
              * @type {list}
              */
@@ -93,7 +100,28 @@ function JSONForceGraph(props) {
                 .append("circle")
                 .attr("r", 5)
                 .attr("fill", function () {
-                    return "red";
+                    return "purple";
+                })
+                .on('mouseover', function (d, i) {
+                    d3.select(this).transition()
+                        .duration('50')
+                        .attr('opacity', '.85');
+                    div.transition()
+                        .duration(50)
+                        .style("opacity", 1);
+                    let num = ("id: " + d.name);
+                    console.log("the value of d is: " + d.name)
+                    div.html(num)
+                        .style("left", (d3.event.pageX + 10) + "px")
+                        .style("top", (d3.event.pageY - 15) + "px");
+                })
+                .on('mouseout', function (d, i) {
+                    d3.select(this).transition()
+                        .duration('50')
+                        .attr('opacity', '1');
+                    div.transition()
+                        .duration('50')
+                        .style("opacity", 0);
                 })
                 .call(
                     d3
